0c06ccb4d796da74f5d20048a1179830ecc167b2
Atikur Rahman <mratikurrahman@gmail.com>
2023-09-16 22:04:48 +0600

    Test the decoder

--- Earthquakes/Models/Quake.swift
+++ Earthquakes/Models/Quake.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Quake: Identifiable {
+struct Quake {
     let magnitude: Double
     let place: String
     let time: Date
---
+++ EarthquakesTests/EarthquakesTests.swift
@@ -0,0 +1,25 @@
+//
+//  EarthquakesTests.swift
+//  EarthquakesTests
+//
+//  Created by Atikur Rahman on 9/16/23.
+//
+
+import XCTest
+@testable import Earthquakes
+
+final class EarthquakesTests: XCTestCase {
+
+    func testGeoJSONDecoderDecodesQuake() throws {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .millisecondsSince1970
+        let quake = try decoder.decode(Quake.self, from: testFeature_nc73649170)
+        
+        XCTAssertEqual(quake.code, "73649170")
+        
+        let expectedSeconds = TimeInterval(1636129710550) / 1000
+        let decodedSeconds = quake.time.timeIntervalSince1970
+        
+        XCTAssertEqual(expectedSeconds, decodedSeconds, accuracy: 0.00001)
+    }
+}
