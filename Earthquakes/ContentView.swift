342e4524fd9d8f523cfa6bd20898b1ec1caa9ff5
Atikur Rahman <mratikurrahman@gmail.com>
2023-09-16 06:01:44 +0600

    Project created

---
+++ Earthquakes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Earthquakes
+//
+//  Created by Atikur Rahman on 9/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Atikur Rahman on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
