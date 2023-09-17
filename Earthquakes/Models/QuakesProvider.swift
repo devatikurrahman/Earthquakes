7dfc01ae5742217a7ed4cb5dd7a284c878407201
Atikur Rahman <mratikurrahman@gmail.com>
2023-09-17 22:15:13 +0600

    View client data

---
+++ Earthquakes/Models/QuakesProvider.swift
@@ -0,0 +1,28 @@
+//
+//  QuakesProvider.swift
+//  Earthquakes
+//
+//  Created by Atikur Rahman on 9/17/23.
+//
+
+import Foundation
+
+class QuakesProvider: ObservableObject {
+    
+    @Published var quakes: [Quake] = []
+    
+    let client: QuakeClient
+    
+    func fetchQuakes() async throws {
+        let latestQuakes = try await client.quakes
+        self.quakes = latestQuakes
+    }
+    
+    func deleteQuakes(atOffsets offsets: IndexSet) {
+        quakes.remove(atOffsets: offsets)
+    }
+    
+    init(client: QuakeClient = QuakeClient()) {
+        self.client = client
+    }
+}
